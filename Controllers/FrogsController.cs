ceada727e31e25572c5148fe816a8d0e43381012
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-14 11:12:22 -0700

    started get request on frogs controller

--- Controllers/FrogsController.cs
+++ Controllers/FrogsController.cs
@@ -4,9 +4,24 @@ namespace froggy_finder_api.Controllers;
 [Route("api/frogs")] // super('api/frogs')
 public class FrogsController : ControllerBase // FrogsController extends BaseController
 {
-  [HttpGet] // .get('', this.test)
+  [HttpGet("test")] // .get('/test', this.test)
   public string Test()
   {
     return "Frogs Controller is working!";
+  }
+
+  // NOTE ActionResult represents an HTTP response with a List of Frog as the response body
+  [HttpGet]
+  public ActionResult<List<Frog>> GetAllFrogs() // .get('', this.getAllFrogs)
+  {
+    try
+    {
+      List<Frog> frogs = [];
+      return Ok(frogs);
+    }
+    catch (Exception error)
+    {
+      return BadRequest(error.Message); // next(error)
+    }
   }
 }
