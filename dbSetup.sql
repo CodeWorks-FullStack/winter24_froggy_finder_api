b2ceedefb85ffa0cd7447eee55b9143001d394ae
Jeremy Fowler <jembyfowler@gmail.com>
2025-01-14 10:34:49 -0700

    sql stuff

--- dbSetup.sql
+++ dbSetup.sql
@@ -1,20 +1,34 @@
 -- Active: 1736871711556@@127.0.0.1@3306@adaptable_shaman_540684_db
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) UNIQUE COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8mb4 COMMENT '';
 
-
+-- adds table to database where we can store rows of data. usually your first step for a project
 CREATE TABLE frogs(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name TINYTEXT NOT NULL,
   species ENUM('tree', 'toad', 'desert', 'goliath', 'regular') NOT NULL,
   size INT UNSIGNED NOT NULL,
-  is_poisonous BOOLEAN NOT NULL DEFAULT false
-);
+  is_poisonous BOOLEAN NOT NULL DEFAULT false,
+  bio TEXT NOT NULL
+) DEFAULT CHARSET utf8mb4;
 
+-- removes the entire table
 DROP TABLE frogs;
+
+
+-- inserts rows of data into table
+INSERT INTO 
+frogs(species, name, size, is_poisonous, bio)
+VALUES
+('desert', 'stinky', 10, false, "I eat garbage"),
+('tree', 'marble', 4,true, "I eat flies and poison hikers"),
+('tree', 'terry', 3,false, "I eat chili dogs"),
+('regular', 'mick', 120,false, "I like long hops on the beach");
+
+-- removes all rows from table 
+DELETE FROM frogs;
+
+-- removes a specific row from a table 
+DELETE FROM frogs WHERE id = 1;
+
+SELECT * FROM frogs;
+SELECT * FROM frogs WHERE id = 6;
+SELECT *, true AS hates_snakes FROM frogs WHERE size > 5;
